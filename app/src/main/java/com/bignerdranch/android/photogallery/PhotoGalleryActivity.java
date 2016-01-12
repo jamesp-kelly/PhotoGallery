71b328d96f068314b9f46a496de2528a09dd8996
James Kelly <jk_jameskelly@outlook.com>
2016-01-12 11:53:28 +0000

    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+
+
+}
